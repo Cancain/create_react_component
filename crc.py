9d00b21f057ef856563344777e5f3584efdc8c68
Cancain <eriksson.tomas@gmail.com>
2019-09-28 17:09:10 +0200

    first commit

---
+++ crc.py
@@ -0,0 +1,22 @@
+def isTypeScript():
+    ansver = input("tsx or js? ")
+    if ansver == "tsx":
+        return ".tsx"
+    if ansver == "js":
+        return ".js"
+    else:
+        return ""
+
+
+componentName = input("Name of coponent? ")
+fileExtension = isTypeScript()
+
+while fileExtension == "":
+    print("try again ")
+    fileExtension = isTypeScript()
+
+filename = componentName + fileExtension
+f = open(filename, "w+")
+f.write("import React from 'react'\n\n")
+exportLine = "export default function {}".format(componentName)
+f.write(exportLine + "{\n\n }")
